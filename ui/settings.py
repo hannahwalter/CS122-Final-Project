2247a20f9a76cf8f5224daf707dc5503f308d7dd
linsucnbeijing <sul@uchicago.edu>
2017-03-09 12:29:52 -0600

    improved django

--- ui/settings.py
+++ ui/settings.py
@@ -93,6 +93,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = '<your_path>/media'
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
